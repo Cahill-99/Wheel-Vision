4a368f3f5d19bc2f983cb1d6968c9275d02c1978
ERIC <ecahill001@gmail.com>
2021-06-26 17:13:13 -0230

    Added carousel

---
+++ index.js
@@ -0,0 +1,52 @@
+
+//***** CAROUSEL ******
+
+const uparrow = document.querySelector(".uparrowimg");
+const downarrow = document.querySelector(".downarrowimg");
+
+const track = document.querySelector(".track");
+const trackHeight = document.querySelector(".track").offsetHeight;
+
+const carouselHeight = document.querySelector(".carousel-container").offsetHeight;
+
+let index = 0;
+
+//TEST ZONE
+console.log(carouselHeight);
+
+//UPARROW
+uparrow.addEventListener("click", () => {
+    index--
+    track.style.transform = `translateY(-${index * carouselHeight}px)`
+    console.log(trackHeight - (index * carouselHeight));
+    downarrow.classList.remove("hide");
+
+    if(index === 0) {
+        uparrow.classList.remove("show");
+    }
+})
+
+//DOWNARROW
+downarrow.addEventListener("click", () => {
+    index++;
+    uparrow.classList.add("show");
+    track.style.transform = `translateY(-${index * carouselHeight}px)`
+    uparrow.classList.add("show");
+    console.log(trackHeight - (index * carouselHeight))
+
+    if (trackHeight - (index * carouselHeight) <= carouselHeight + 2) {
+       downarrow.classList.add("hide");
+
+    }
+})
+
+
+//***** WHEEL SHOW AND HIDE *****
+
+let shownWheels = document.querySelector(".displayed-wheels");
+let wheelChoice = document.querySelector(".wheel2img");
+
+wheelChoice.addEventListener("click", () => {
+    shownWheels.classList.toggle("show");
+    wheelChoice.classList.toggle("border");
+})
