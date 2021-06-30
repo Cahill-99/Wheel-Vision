41bc484088584c2b18e7c307fe0dde87706c48e7
ERIC <ecahill001@gmail.com>
2021-06-30 19:54:06 -0230

    Made front wheel moveable

--- index.js
+++ index.js
@@ -52,3 +52,40 @@ wheelChoice.addEventListener("click", () => {
     shownWheels.classList.toggle("show");
     wheelChoice.classList.toggle("border");
 })
+
+
+//***** DRAGGABLE WHEELS *****
+
+
+//FRONT WHEEL
+const el = document.querySelector(".front-wheel");
+
+el.addEventListener("mousedown", mouseDown);
+
+function mouseDown(e) {
+    window.addEventListener("mousemove", mouseMove);
+    window.addEventListener("mouseup", mouseUp);
+
+    let prevX = e.clientX;
+    let prevY = e.clientY;
+
+    function mouseMove(e) {
+        let newX = prevX - e.clientX;
+        let newY = prevY - e.clientY;
+
+        const rect = el.getBoundingClientRect();
+
+        el.style.left = rect.left - newX + "px";
+        el.style.top = rect.top - newY + "px";
+
+        prevX = e.clientX;
+        prevY = e.clientY;
+    }
+
+    function mouseUp() {
+        window.removeEventListener("mousemove", mouseMove);
+        window.removeEventListener("mouseup", mouseUp);
+    }
+}
+
+
