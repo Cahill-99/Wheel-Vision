60e118bcd7d53b72b4f865fec50695bd9d79b7e6
ERIC <ecahill001@gmail.com>
2021-08-01 16:22:10 -0230

    added Math.ceil method to function to fix arrow, and separated wheelsArray to separate file

--- index.js
+++ index.js
@@ -11,16 +11,20 @@ const carouselHeight = document.querySelector(".carousel-container").offsetHeigh
 let loader = document.querySelector('.loading')
 let bgBox = document.querySelector('.carimage')
 
+let body = document.getElementsByTagName("BODY")[0];
+
 const loadRemove = () => {
     console.log("loaded");
     loader.classList.add("hide");
 }
 
+body.onload = () => {
+loadRemove()
+}
 
 
-let index = 0;
 
-//TEST ZONE
+
 
 
 //*** SIDEBAR WHEELS ***
@@ -28,128 +32,18 @@ let index = 0;
 
 
 //WHEEL OBJECTS ARRAY
-let wheelsArray = [
-        
-    {
-        idName: "wheel1img",
-        imgSource:"img/EnkeiGTC02SilverGC.png",
-        colour: "silver",
-        spokes: 6,
-        brand: "enkei",
-        backImage: "url('img/Chosen Wheels/Wheel1 installed-min.jpg')",
-        temp: 'img/Chosen Wheels/Wheel1 installed-min.jpg'
-    },
-    {
-        idName: "wheel2img",
-        imgSource:"img/WorkMeisterL1GunmetalGC.png",
-        colour: "grey",
-        spokes: 6,
-        brand: "work",
-        backImage: "url('img/Chosen Wheels/Wheel2 installed-min.jpg')",
-        temp: 'img/Chosen Wheels/Wheel2 installed-min.jpg'
-    },
-    {
-        idName: "wheel3img",
-        imgSource:"img/3SDM005MachinedGC.png",
-        colour: "silver",
-        spokes: 5,
-        brand: "3SDM",
-        backImage: "url('img/Chosen Wheels/Wheel3 installed-min.jpg')",
-        temp: 'img/Chosen Wheels/Wheel3 installed-min.jpg'
-    },
-    {
-        idName: "wheel4img",
-        imgSource:"img/EnkeiTM7GreyGC.png",
-        colour: "silver",
-        spokes: "6+",
-        brand: "enkei",
-        backImage: "url('img/Chosen Wheels/Wheel4 installed-min.jpg')",
-        temp: 'img/Chosen Wheels/Wheel4 installed-min.jpg'
-    },
-    {
-        idName: "wheel5img",
-        imgSource:"img/EnkeiTM7BlackGC.png",
-        colour: "black",
-        spokes: "6+",
-        brand: "enkei",
-        backImage: "url('img/Chosen Wheels/Wheel5 installed-min.jpg')",
-        temp: 'img/Chosen Wheels/Wheel5 installed-min.jpg'
-    },
-    {
-        idName: "wheel6img",
-        imgSource:"img/EnkeiRPF1BlackChromeGC.png",
-        colour: "grey",
-        spokes: 6,
-        brand: "enkei",
-        backImage: "url('img/Chosen Wheels/Wheel6 installed-min.jpg')",
-        temp: 'img/Chosen Wheels/Wheel6 installed-min.jpg'
-    },
-    {
-        idName: "wheel7img",
-        imgSource:"img/EnkeiRPF1GoldGC.png",
-        colour: "gold",
-        spokes: 6,
-        brand: "enkei",
-        backImage: "url('img/Chosen Wheels/Wheel7 installed-min.jpg')",
-        temp: 'img/Chosen Wheels/Wheel7 installed-min.jpg'
-    },
-    {
-        idName: "wheel8img",
-        imgSource:"img/EnkeiRPF1SilverGC.png",
-        colour:"white",
-        spokes: 6,
-        brand: "enkei",
-        backImage: "url('img/Chosen Wheels/Wheel8 installed-min.jpg')",
-        temp: 'img/Chosen Wheels/Wheel8 installed-min.jpg'
-    },
-    {
-        idName: "wheel9img",
-        imgSource:"img/RaysTE37Gravel2GC.png",
-        colour: "white",
-        spokes: 6,
-        brand: "rays",
-        backImage: "url('img/Chosen Wheels/Wheel9 installed-min.jpg')",
-        temp: 'img/Chosen Wheels/Wheel9 installed-min.jpg'
-    },
-    {
-        idName: "wheel10img",
-        imgSource:"img/VolkG16GC.png",
-        colour: "grey",
-        spokes: "6+",
-        brand: "rays",
-        backImage: "url('img/Chosen Wheels/Wheel10 installed-min.jpg')",
-        temp: 'img/Chosen Wheels/Wheel10 installed-min.jpg'
-    },
-    {
-        idName: "wheel11img",
-        imgSource:"img/WorkCRKiwamiBlack.png",
-        colour: "black",
-        spokes: 5,
-        brand: "work",
-        backImage: "url('img/Chosen Wheels/Wheel11 installed-min.jpg')",
-        temp: 'img/Chosen Wheels/Wheel11 installed-min.jpg'
-    },
-    {
-        idName: "wheel12img",
-        imgSource:"img/WorkCRKiwamiSilverGC.png",
-        colour: "silver",
-        spokes: 5,
-        brand:"work",
-        backImage: "url('img/Chosen Wheels/Wheel12 installed-min.jpg')",
-        temp: 'img/Chosen Wheels/Wheel12 installed-min.jpg'
-    }
-]
+
+import { wheelsArray } from "./wheels.js"
 
 
 //LOOP THROUGH WHEELS ARRAY
 
-
-    for(let i = 0 ; i < wheelsArray.length; i++) {
+    wheelsArray.forEach(wheel => {
         let newWheel = document.createElement("IMG");
         
-        newWheel.src = wheelsArray[i].imgSource;
+        newWheel.src = wheel.imgSource;
         newWheel.classList.add("wheelstandard");
-        newWheel.id = wheelsArray[i].idName;
+        newWheel.id = wheel.idName;
         track.appendChild(newWheel);
         console.log(newWheel.id);
 
@@ -158,28 +52,28 @@ let wheelsArray = [
             
             loader.classList.remove("hide");
             let tempImage = new Image(200,100);
-            tempImage.src = wheelsArray[i].temp;
+            tempImage.src = wheel.temp;
             tempImage.onload = () => {
-                bgImage.style.backgroundImage = wheelsArray[i].backImage;
+                bgImage.style.backgroundImage = wheel.backImage;
                 loader.classList.add("hide");
                 tempImage.remove();
             }
-            console.log("clicked");
-            console.log(tempImage.src);
 
         }
         
-    }
-        
-    //This has messed up the "trackHeight" measurement for some reason
+
+    });
 
 
+let index = 0;
+let wheelDivision = wheelsArray.length/4;
+let roundedDivision = Math.ceil(wheelDivision);
+console.log(roundedDivision);
 
 //UPARROW
 uparrow.addEventListener("click", () => {
     index--
     track.style.transform = `translateY(-${index * carouselHeight}px)`
-    console.log(trackHeight - (index * carouselHeight));
     downarrow.classList.remove("hide");
 
     if(index === 0) {
@@ -192,12 +86,11 @@ downarrow.addEventListener("click", () => {
     index++;
     uparrow.classList.add("show");
     track.style.transform = `translateY(-${index * carouselHeight}px)`;
-    console.log(trackHeight - (index * carouselHeight))
-    console.log("carouselHieght" + carouselHeight);
-    if (trackHeight - (index * carouselHeight) <= carouselHeight + 10) {
-       downarrow.classList.add("hide");
-
+    
+    if (roundedDivision - 1 === index) {
+        downarrow.classList.add("hide");
     }
+    
 })
 
 
